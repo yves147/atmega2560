5a46b20299306d0a0c6fead1336e0fa31e764889
yves147 <yves.j@outlook.de>
2022-08-20 23:45:33 +0200

    Parametisierung und Fehlerbehebung in Upload Script Co-authored-by: Richard <roeald@users.noreply.github.com>

--- upload.sh
+++ upload.sh
@@ -1,16 +1,22 @@
 c_ext=".c"
 ard_dev="/dev/ttyACM0"
 ard_3="115200"
-if [[ "$1" =~ "$c_ext" ]]
+
+arduino_install_path="/home/yves/Downloads/arduino-1.8.19-linux64/arduino-1.8.19"
+running_path="/home/yves/Schreibtisch/mega2560-fun"
+
+# already built disassembler and dotnet-runtime-3.1 required
+avrdisassembler_path="/home/yves/Downloads/AVRDisassembler"
+
+programn=$1;
+
+avr-gcc -Os -DF_APU=16000000UL -mmcu=atmega2560 -c -o tmp/${programn}.o ${programn}.c
+avr-gcc -mmcu=atmega2560 tmp/${programn}.o -o tmp/${programn}.bin
+avr-objcopy -O ihex -R .eeprom tmp/${programn}.bin tmp/${programn}.hex
+
+if [[ $avrdisassembler_path != "" ]]
 then
-    program_name="${1//$c_ext/}"
-else
-    program_name=$1;
+${avrdisassembler_path}/Source/AVRDisassembler/AVRDisassembler/bin/release/netcoreapp3.1/AVRDisassembler -i ${running_path}/tmp/${programn}.hex > ${running_path}/tmp/${programn}.dasm
 fi
 
-avr-gcc -Os -DF_APU=16000000UL -mmcu=atmega2560 -c -o tmp/${program_name}.o ${program_name}.c
-avr-gcc -mmcu=atmega2560 tmp/${program_name}.o -o tmp/${program_name}.bin
-avr-objcopy -O ihex -R .eeprom tmp/${program_name}.bin tmp/${program_name}.hex
-#sudo avrdude -C/home/yves/Downloads/arduino-1.8.19-linux64/arduino-1.8.19/hardware/tools/avr/etc/avrdude.conf -F -V -c arduino -p atmega2560 -cwiring -P $ard_dev -b $ard_3 -U flash:w:${program_name}.hex
-#avrdude -C/home/yves/Downloads/arduino-1.8.19-linux64/arduino-1.8.19/hardware/tools/avr/etc/avrdude.conf -v -patmega2560 -cwiring -P/dev/ttyACM0 -b115200 -D -Uflash:w:/tmp/arduino_build_103871/sketch_aug20a.ino.hex:i 
-sudo /home/yves/Downloads/arduino-1.8.19-linux64/arduino-1.8.19/hardware/tools/avr/bin/avrdude -C/home/yves/Downloads/arduino-1.8.19-linux64/arduino-1.8.19/hardware/tools/avr/etc/avrdude.conf -v -patmega2560 -cwiring -P/dev/ttyACM0 -b115200 -D -Uflash:w:$tmp/{program_name}.hex:i
+sudo ${arduino_install_path}/hardware/tools/avr/bin/avrdude -C${arduino_install_path}/hardware/tools/avr/etc/avrdude.conf -v -patmega2560 -cwiring -P/dev/ttyACM0 -b115200 -D -Uflash:w:tmp/${programn}.hex:i
